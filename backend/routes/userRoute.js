3f29a4d042aa453f1e19b172d169ac95f992d602
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-17 17:25:16 +0530

    signup created

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const zod = require("zod")
+const zod = require("zod");
+const { User } = require("../db");
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken"); 
 const userRoute = express.Router();
 
 
@@ -9,8 +12,42 @@ const signupSchema = zod.object({
   firstName: zod.string(),
   lastName: zod.string()
 })
-userRoute.post("/signup", (req, res) => {
-  const {success} = signupSchema.safeParse(req.body)
+
+// Sign up
+userRoute.post("/signup", async (req, res) => {
+  const {success} = signupSchema.safeParse(req.body);
+  if(!success){
+    res.status(411).json({
+      messege: "Email already taken / Incorrect inputs"
+    })
+  }
+
+  const existingUser = await User.findOne({
+    username: req.body.username
+  })
+
+  if(existingUser){
+    res.status(411).json({
+      messege: "Email already exist"
+    })
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  })
+
+  const userId = user._id;
+
+  const token = jwt.sign({
+    userId
+  }, JWT_SECRET);
+  res.json({
+    messege: "User created successfully",
+    token: token
+  })
 })
 
 userRoute.post("/signin", (req, res) => {

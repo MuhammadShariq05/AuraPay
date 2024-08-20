42efbd4f3d85dc8b8aab21bccd199141ce700f5c
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-20 19:04:13 +0530

    account file added

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+// const bcrypt = require("bcrypt");
 
 // connect
 mongoose.connect("mongodb://localhost:27017/paytm");
@@ -6,15 +7,41 @@ mongoose.connect("mongodb://localhost:27017/paytm");
 // schema
 const userSchema = mongoose.Schema({
   username: String,
-  password: String,
   firstName: String,
   lastName: String,
+  password: String
 })
 
+const accountSchema = mongoose.Schema({
+  userID: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+})
+// userSchema.methods.createHash = async function(plainTextPassword){
+//   // / Hashing user's salt and password with 10 iterations,
+//   const saltRounds = 10;
+
+//   const salt = await bcrypt.genSalt(saltRounds);
+//   return await bcrypt.hash(plainTextPassword, salt);
+// }
+
+// // validating the canditate password with stored hash and hash function
+// userSchema.methods.validatePassword = async function (canditatePassword) {
+//   return await bcrypt.compare(canditatePassword, this.password_hash)
+// }
+
 // model
+const Account = mongoose.model("Account", accountSchema)
 const User = mongoose.model("User",  userSchema);
 
 
 module.exports = {
-  User
-}
+  User,
+  Account
+  }
---
+++ backend/routes/accountRouter.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { authMiddleware } = require("../middleware");
+const { Account } = require("../db");
+const { startSession } = require("mongoose");
+const router = express.Router();
+
+router.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+
+  res.json({
+    balance: account.balance,
+  });
+});
+
+router.post("/transfer", authMiddleware, async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
+
+  const { amount, to } = req.body;
+
+  // Fetch the account with the transaction
+  const account = await Account.findOne({ userId: req.userId }).session(
+    session
+  );
+
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      messege: "Insufficient balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({ userId: to }).session(session);
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      messege: "Invalid account",
+    });
+  }
+
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
+  await Account.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
+
+  await session.commitTransaction();
+  res.status(200).json({
+    messege: "transfer successful",
+  });
+});
+
+module.exports = {
+  router,
+};

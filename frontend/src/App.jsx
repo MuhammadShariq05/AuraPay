3a98bafac164ad0dde9fb06e87ae8a6cfb3cc2a3
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-09-04 10:14:37 +0530

    added boiler plates routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { Siginup } from './pages/Siginup'
+import { Signin } from './pages/signin'
+import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/SendMoney'
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <div className='bg-red-500'>Home page</div>
+      {console.log("line 17")}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Siginup />}/>
+          <Route path='/signin' element={<Signin />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/send' element={<SendMoney />}/>
+        </Routes>
+      </BrowserRouter>
+      {console.log("line 26")}
+    </>
   )
 }
 

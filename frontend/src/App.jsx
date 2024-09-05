a47a91ca15a34323dffa2550f4c564420cbc491c
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-09-05 15:18:31 +0530

    Appbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { HomePage } from './pages/HomePage'
 import { Siginup } from './pages/Siginup'
 import { Signin } from './pages/signin'
 import { Dashboard } from './pages/Dashboard'
@@ -11,6 +12,7 @@ function App() {
       {console.log("line 17")}
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<HomePage />}/>
           <Route path='/signup' element={<Siginup />}/>
           <Route path='/signin' element={<Signin />}/>
           <Route path='/dashboard' element={<Dashboard />}/>
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AppBar } from '../components/AppBar'
+
+export const HomePage = () => {
+  // Appbar
+  // icons
+  // animation 
+  // 
+
+  return (
+    <div className='flex'>
+      <AppBar />
+    </div>
+  )
+}
